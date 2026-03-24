45dacdae9e11206309c7ed4ee55ab9873aacbea7
Askhat Berik <askiberi@gmail.com>
2026-03-24 18:45:56 -0500

    Update HelloWorld11.java
    added lines

--- src/day01/HelloWorld11.java
+++ src/day01/HelloWorld11.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld11 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello USA");
+        System.out.println("Hello KGZ");
+        
     }
 }
